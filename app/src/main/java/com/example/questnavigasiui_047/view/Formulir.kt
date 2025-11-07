69fc12228999acbf34b82aff3dab0cd5f07ddcf8
ibnatysalsabila <salsajpr01@gmail.com>
2025-11-07 15:43:12 +0700

    menambahkan field input untuk alamat

--- app/src/main/java/com/example/questnavigasiui_047/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_047/view/Formulir.kt
@@ -85,3 +85,11 @@ fun FromIsian(
                 thickness = 1.dp,
                 color = Color.Red
             )
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .width(width = 250.dp),
+                label = {Text(text = "Alamat")},
+                onValueChange = {},
+            )
